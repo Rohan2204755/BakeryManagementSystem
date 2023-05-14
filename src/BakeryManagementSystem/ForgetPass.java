b357b61093ecdb2601e34c92aadb9e2d82778bb1
anjilakanwar <np03cs4a210002@heraldcollege.edu.np>
2023-05-14 14:18:45 +0545

    issues #10

--- src/BakeryManagementSystem/ForgetPass.java
+++ src/BakeryManagementSystem/ForgetPass.java
@@ -13,6 +13,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.JOptionPane and java.sql.SQLException;
 
 /**
  *
